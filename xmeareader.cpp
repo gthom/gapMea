04725b7b51ff328e8d6155e3cc7881295e80adca
Gilles Thomassin <gthomassingap@gmail.com>
2016-12-30 15:54:03 +0100

    Ajout des infos copyright et licence à chaque fichier

--- xmeareader.cpp
+++ xmeareader.cpp
@@ -1,3 +1,20 @@
+/*
+« Copyright 2016 Gilles Thomassin <gthomassingap@gmail.com> »
+
+ This file is part of GapMea.
+
+    GapMea is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, version 3.
+
+    GapMea is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with GapMea.  If not, see <http://www.gnu.org/licenses/>.
+*/
 #include "xmeareader.h"
 #include "lien.h"
 #include "lienreflexif.h"
